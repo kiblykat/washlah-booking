0d73c765026e8bab617140c9b2a27dbd97d63730
izzat <izzatfadzlon@gmail.com>
2024-09-03 18:15:52 +0800

    fix signup handling

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import { doSignInWithEmailAndPassword } from "../firebase/auth";
 import toast, { Toaster } from "react-hot-toast";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/hooks";
 
 export default function Login() {
   const { userLoggedIn } = useAuth();
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     try {
@@ -20,6 +21,11 @@ export default function Login() {
       toast.error("Invalid login details");
     }
   };
+
+  function navigateSignup() {
+    navigate("signup");
+  }
+
   return (
     <>
       {userLoggedIn && <Navigate to="/home/book" replace={true} />}
@@ -64,9 +70,12 @@ export default function Login() {
               <Toaster />
             </form>
             <div className="text-center mt-4">
-              <a href="#" className="link-hover link-primary">
+              <button
+                onClick={() => navigateSignup()}
+                className="link-hover link-primary"
+              >
                 Sign Up
-              </a>
+              </button>
             </div>
           </div>
         </div>
