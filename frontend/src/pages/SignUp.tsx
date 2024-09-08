ba4f7f8fb1d7de809f9444336cae2b1be68e8428
izzat <izzatfadzlon@gmail.com>
2024-09-08 23:39:49 +0800

    update signup page to auto navigate

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,45 +1,53 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/hooks";
+
 export default function SignUp() {
+  const { userLoggedIn } = useAuth();
+
   return (
-    <div className="flex h-screen justify-center items-center bg-gray-100">
-      <div className="w-full max-w-sm">
-        <div className="card bg-white shadow-lg p-8">
-          <h2 className="text-2xl font-bold text-center mb-6">Sign Up</h2>
-          <form>
-            <div className="form-control mb-4">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                className="input input-bordered w-full"
-              />
-            </div>
-            <div className="form-control mb-6">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                className="input input-bordered w-full"
-              />
-            </div>
-            <div className="form-control mb-6">
-              {/* <label className="label cursor-pointer justify-start space-x-2">
+    <>
+      {userLoggedIn && <Navigate to="/home/book" replace={true} />}
+      <div className="flex h-screen justify-center items-center bg-gray-100">
+        <div className="w-full max-w-sm">
+          <div className="card bg-white shadow-lg p-8">
+            <h2 className="text-2xl font-bold text-center mb-6">Sign Up</h2>
+            <form>
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="input input-bordered w-full"
+                />
+              </div>
+              <div className="form-control mb-6">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  className="input input-bordered w-full"
+                />
+              </div>
+              <div className="form-control mb-6">
+                {/* <label className="label cursor-pointer justify-start space-x-2">
                 <input type="checkbox" className="checkbox" />
                 <span className="label-text">Remember me</span>
               </label> */}
+              </div>
+              <button className="btn btn-primary w-full">Sign Up</button>
+            </form>
+            <div className="text-center mt-4">
+              <a href="#" className="link-hover link-primary">
+                Login
+              </a>
             </div>
-            <button className="btn btn-primary w-full">Sign Up</button>
-          </form>
-          <div className="text-center mt-4">
-            <a href="#" className="link-hover link-primary">
-              Login
-            </a>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
