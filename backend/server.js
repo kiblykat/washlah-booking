1a891af9525337b045352f67c7d7dfb4f62fdcf5
izzat <izzatfadzlon@gmail.com>
2024-09-12 01:34:42 +0800

    be: comment to force deployment

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//use /tickets endpoints for ticket booking
 app.use("/tickets", ticketRoutes);
 
 const startServer = async () => {
